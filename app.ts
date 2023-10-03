7e4bbb70c938b71827b945d6cd2d1a31be683e12
Bernardo Boé del Castillo <111295043+bernardoboe@users.noreply.github.com>
2023-10-03 17:14:33 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,17 @@
+﻿import app = require("teem");
+
+app.run({
+	root: "/yatchlife",
+	// Configurações de acesso ao banco de dados.
+	// Mais informações: https://www.npmjs.com/package/mysql2#using-connection-pools
+	sqlConfig: {
+		connectionLimit: 30,
+		waitForConnections: true,
+		charset: "utf8mb4",
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: "root",
+		database: "embarcacoes"
+	}
+});
